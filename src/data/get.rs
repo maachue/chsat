f8a7308f09a28e2025eec7e14d23cd081865669e
Maachue <109926858+maachue@users.noreply.github.com>
2026-02-05 19:40:46 +0700

    feat: data improving

--- src/data/get.rs
+++ src/data/get.rs
@@ -1,9 +1,9 @@
 use std::{path::Path, process::Command};
 
-use crate::data::{KernelInfo, OSReleaseInfo};
+use crate::data::{KernelInfo, OSReleaseInfo, PackageManagerInfo, WindowsInfo};
 
 use super::ExposeData;
-use color_eyre::{Result, eyre::OptionExt};
+use color_eyre::Result;
 
 impl KernelInfo {
     pub fn get() -> Result<Self> {
@@ -12,12 +12,14 @@ impl KernelInfo {
             Ok(Self {
                 osrelease: sys_info::os_release().unwrap_or("".to_string()),
                 ostype: sys_info::os_type().unwrap_or("".to_string()),
-                version: String::from_utf8(Command::new("uname").arg("-s").output()?.stdout)
-                    .unwrap_or("".to_string()),
+                version: String::from_utf8(Command::new("uname").arg("-v").output()?.stdout)
+                    .unwrap_or_default()
+                    .trim() // Remove `\n`
+                    .to_string(),
             })
         }
 
-        #[cfg(target_family = "windows")]
+        #[cfg(not(target_family = "unix"))]
         Ok(Self::default())
     }
 }
@@ -26,40 +28,81 @@ impl OSReleaseInfo {
     pub fn get() -> Result<Self> {
         #[cfg(target_family = "unix")]
         {
-            let sys = sys_info::linux_os_release()?;
+            let os = sys_info::linux_os_release()?;
             Ok(Self {
-                bug_report_url: sys.bug_report_url.unwrap_or("".to_string()),
-                build_id: sys.build_id.unwrap_or("".to_string()),
-                documentation_url: sys.documentation_url.unwrap_or("".to_string()),
-                home_url: sys.home_url.unwrap_or("".to_string()),
-                id: sys.id.unwrap_or("".to_string()),
-                logo: sys.logo.unwrap_or("".to_string()),
-                name: sys.name.unwrap_or("".to_string()),
-                pretty_name: sys.pretty_name.unwrap_or("".to_string()),
-                privacy_policy_url: sys.privacy_policy_url.unwrap_or("".to_string()),
-                support_url: sys.support_url.unwrap_or("".to_string()),
+                bug_report_url: os.bug_report_url.unwrap_or_default(),
+                build_id: os.build_id.unwrap_or_default(),
+                documentation_url: os.documentation_url.unwrap_or_default(),
+                home_url: os.home_url.unwrap_or_default(),
+                id: os.id.unwrap_or_default(),
+                logo: os.logo.unwrap_or_default(),
+                name: os.name.unwrap_or_default(),
+                pretty_name: os.pretty_name.unwrap_or_default(),
+                privacy_policy_url: os.privacy_policy_url.unwrap_or_default(),
+                support_url: os.support_url.unwrap_or_default(),
             })
         }
-        #[cfg(target_family = "windows")]
+        #[cfg(not(target_family = "unix"))]
         Ok(Self::default())
     }
 }
 
+impl WindowsInfo {
+    pub fn get() -> Self {
+        /// TODO: add windows specific infomation
+        #[cfg(target_family = "windows")]
+        {
+            Ok(Self {})
+        }
+
+        #[cfg(not(target_family = "windows"))]
+        Self::default()
+    }
+}
+
+impl PackageManagerInfo {
+    pub fn get(distro_id: &str) -> Self {
+        match distro_id {
+            "cachyos" | "archlinux" | "endeavouros" => Self {
+                binary_name: "pacman".to_string(),
+                binary_path: which::which("pacman").unwrap_or_default(),
+                flags: super::PMFlags {
+                    install_flags: vec!["-S".to_string()],
+                    remove_flags: vec!["-Rns".to_string()],
+                    update_pkgs_flags: vec!["-Syu".to_string()],
+                    update_metadata_flags: vec!["-Syy".to_string()],
+                },
+                usage: "{{pm}} {{flags}} {{pkgs}} {{opts}}".to_string(),
+            },
+            _ => Self::default(),
+        }
+    }
+}
+
 impl ExposeData {
-    pub fn new(args: Vec<String>, config: &Path) -> Result<Self> {
+    pub fn new(args: &[String], config: &Path) -> Result<Self> {
+        let os_release = OSReleaseInfo::get()?;
+
+        let os_id = if std::env::consts::OS == "windows" {
+            std::env::consts::OS
+        } else {
+            &os_release.id
+        };
+
         Ok(Self {
             chsat: super::ChsatData {
-                arch: "amd64",
-                args,
+                arch: std::env::consts::ARCH,
+                args: args.to_owned(),
             },
             command_dir: shellexpand::tilde(".").into_owned().into(),
             config_file: config.to_path_buf(),
-            executable: which::which("chsat").unwrap_or("".into()),
-            home_dir: std::env::home_dir().unwrap_or("".into()),
-            hostname: sys_info::hostname().unwrap_or("".to_string()),
+            executable: which::which("chsat").unwrap_or_default(),
+            home_dir: std::env::home_dir().unwrap_or_default(),
+            hostname: sys_info::hostname().unwrap_or_default(),
             kernel: KernelInfo::get()?,
             os: std::env::consts::OS,
-            os_release: OSReleaseInfo::get()?,
+            package_manager: PackageManagerInfo::get(os_id),
+            os_release,
             uid: users::get_current_uid().to_string(),
             username: users::get_current_username()
                 .unwrap_or("".into())
@@ -70,8 +113,7 @@ impl ExposeData {
                 .unwrap_or("".into())
                 .to_string_lossy()
                 .into_owned(),
-            windows_version: super::WindowsInfo::default(),
-            package_manager: super::PackageManagerInfo::default(),
+            windows_version: WindowsInfo::get(),
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 use color_eyre::Result;
 
 use crate::cli::Commands;
 mod cli;
-mod config_toml;
+// mod config_toml;
 mod data;
 mod rhai_impl;
 // mod utils;
@@ -14,7 +16,11 @@ fn main() -> Result<()> {
     // let stdin = utils::read_stdin()?;
 
     match cmd.command {
-        Commands::Data => {}
+        Commands::Data => {
+            let data = data::ExposeData::new(&["data".to_string()], &PathBuf::new())?;
+            let json = serde_json::to_string_pretty(&data)?;
+            println!("{}", json);
+        }
         _ => todo!(),
     }
 
